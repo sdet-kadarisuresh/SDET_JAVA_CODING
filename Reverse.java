9717a19763b7f57bd292561a3b86c0fa38980b29
sdet-kadarisuresh <kadarisureshsdet@gmail.com>
2026-09-12 15:35:36 +0530

    String palindrome

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,18 @@
 public class Reverse {
-
     public static void main(String args[]) {
+        String str = "hello";
 
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(arr);
     }
 }
+
